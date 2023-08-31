4c64224925c03650838b29a3e7120a828fb07317
Smailey <smailey.behm@gmail.com>
2023-08-31 11:19:59 -0300

    ajustando rotas

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,15 +9,15 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to="/dogs" aria-label="Dogs - Home">
+        <Link className={styles.logo} to="/dogs/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
         {data ? (
-          <Link className={styles.login} to="/dogs/conta">
+          <Link className={styles.login} to="dogs/conta">
             {data.nome}
           </Link>
         ) : (
-          <Link className={styles.login} to="/dogs/login">
+          <Link className={styles.login} to="dogs/login">
             Login / Criar
           </Link>
         )}
